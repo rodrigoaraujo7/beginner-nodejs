4f0ad07a26e498b364bc3bb704eecc9f2bfec1d2
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-06-10 16:05:25 -0300

    adding content to an existing file with fs path

--- modules/fs.js
+++ modules/fs.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-// Create folder - create a folder on modules with a test name and give me a callback
+// Create folder - create a folder on modules with a test name and give me a callback!
 fs.mkdir(path.join(__dirname, "/test"), (error) => {
   if (error) {
     return console.log("Error:", error);
@@ -10,7 +10,7 @@ fs.mkdir(path.join(__dirname, "/test"), (error) => {
   console.log("Folder created successfully!");
 });
 
-// Create file - create a file on a different directory with a content and give me a callback
+// Create file - create a file on a different directory with a content and give me a callback!
 fs.writeFile(
   path.join(__dirname, "/test", "app.txt"),
   "hello node!",
@@ -20,5 +20,18 @@ fs.writeFile(
     }
 
     console.log("File created successfully!");
+  }
+);
+
+// Add content to a file - adding content to a existing file without deleting the old content!
+fs.appendFile(
+  path.join(__dirname, "/test", "app.txt"),
+  "hello world!",
+  (error) => {
+    if (error) {
+      return console.log("Error:", error);
+    }
+
+    console.log("Content added successfully!");
   }
 );
