2d7eaaa5b153e8cbdfc8d53ae3231838e6bc285d
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-06-14 21:42:35 -0300

    new route with json using express

--- modules/express.js
+++ modules/express.js
@@ -7,6 +7,21 @@ app.get("/home", (req, res) => {
   res.status(200).send("<h1>Hello World!<h1/>");
 });
 
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name: "John Doe",
+      email: "john.doe@gmail.com",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane.doe@gmail.com",
+    },
+  ];
+
+  res.status(200).json(users);
+});
+
 const port = 8080;
 
 app.listen(port, () => console.log(`💻 Server is running on PORT ${port}!`));
