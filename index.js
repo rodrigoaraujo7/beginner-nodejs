670aed6a19d6d3425a5c3318aa0bd745d84bb371
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-06-14 21:28:11 -0300

    creating a simple sv with http module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Person } = require("./person");
 
 // require("./modules/path");
-require("./modules/fs");
+// require("./modules/fs");
+require("./modules/http");
 
 const person = new Person("Rodrigo");
---
+++ modules/http.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/home") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Home page</h1>");
+  }
+
+  if (req.url === "/users") {
+    const users = [
+      {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+      },
+      {
+        name: "Jane Doe",
+        email: "jane.doe@gmail.com",
+      },
+    ];
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
+
+server.listen(port, () => console.log(`💻 Server is running on PORT ${port}!`));
