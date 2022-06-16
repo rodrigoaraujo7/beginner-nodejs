fb6aa71114fa5e39e6efb44542ec651d705036ad
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-06-15 22:08:53 -0300

    connecting on mongodb with ambient variables

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const { Person } = require("./person");
+// const { Person } = require("./person");
+const dotenv = require("dotenv");
+const connectToDatabase = require("./src/database/connect");
+
+dotenv.config();
+
+connectToDatabase();
 
 // require("./modules/path");
 // require("./modules/fs");
 // require("./modules/http");
 
-require("./modules/express");
+// require("./modules/express");
 
-const person = new Person("Rodrigo");
+// const person = new Person("Rodrigo");
--- src/database/connect.js
+++ src/database/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
   await mongoose.connect(
-    "mongodb+srv://admin:<password>@beginner-nodejs-course.zdeaw.mongodb.net/?retryWrites=true&w=majority",
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@beginner-nodejs-course.zdeaw.mongodb.net/database?retryWrites=true&w=majority`,
     (error) => {
       if (error) {
         return console.log("Error to connect a database:", error);
